ddce5e6e0e34059b57be8a67f8359bd7d8f356cc
HASSANABBASI70 <ha6093917@gmail.com>
2025-12-26 09:50:04 +0500

    big update
    big update

---
+++ app.js
@@ -0,0 +1,55 @@
+
+// var num = parseFloat(prompt("Enter a positive number:"));
+
+// document.write("Number: " + num + "<br>");
+// document.write("Round off value: " + Math.round(num) + "<br>");
+// document.write("Floor value: " + Math.floor(num) + "<br>");
+// document.write("Ceil value: " + Math.ceil(num)
+
+// var num = parseFloat(prompt("Enter a negative number:"));
+
+// document.write("Number: " + num + "<br>");
+// document.write("Round off value: " + Math.round(num) + "<br>");
+// document.write("Floor value: " + Math.floor(num) + "<br>");
+// document.write("Ceil value: " + Math.ceil(num));
+
+
+// var num = parseFloat(prompt("Enter a number:"));
+// document.write("Absolute value: " + Math.abs(num));
+
+
+
+
+// var dice = Math.floor(Math.random() * 6) + 1;
+// document.write("Dice Value: " + dice);
+
+
+// var toss = Math.floor(Math.random() * 2) + 1;
+
+// if (toss === 1) {
+//     document.write("Coin Value: Heads");
+// } else {
+//     document.write("Coin Value: Tails");
+// }
+
+
+// var randomNum = Math.floor(Math.random() * 100) + 1;
+// document.write("Random number between 1 and 100: " + randomNum);
+
+
+// var weight = prompt("Enter your weight:");
+// var parsedWeight = parseFloat(weight);
+
+// document.write("Your weight is: " + parsedWeight + " kgs");
+
+
+
+
+// var secretNumber = Math.floor(Math.random() * 10) + 1;
+// var userGuess = parseInt(prompt("Guess the secret number (1 to 10):"));
+
+// if (userGuess === secretNumber) {
+//     alert("Congratulations! You guessed the secret number.");
+// } else {
+//     alert("Try again! The secret number was " + secretNumber);
+// }
